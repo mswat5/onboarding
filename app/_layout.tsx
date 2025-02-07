c3e027362bdbf111a18882da2ef87aafe0236a7c
mswat5 <mswat14361@gmail.com>
2025-02-07 16:57:24 +0530

    safe area view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native";
 import "../global.css";
 import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
-    <>
-      <StatusBar style="light" />
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}
+    >
+      <StatusBar style="light" translucent={false} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(main)" options={{ headerShown: false }} />
         <Stack.Screen name="index" />
       </Stack>
-    </>
+    </SafeAreaView>
   );
 }
